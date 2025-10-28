dac33c5a60f8d1d9fb4393b8c0abf21a0ca3fd15
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-10-29 00:09:40 +0100

    feat: card! macro

--- src/card/mod.rs
+++ src/card/mod.rs
@@ -4,14 +4,21 @@ use crate::card::value::CardValue;
 pub mod color;
 pub mod value;
 
+#[macro_export]
+macro_rules! card {
+    ($color:tt, $value:tt) => {
+        $crate::card::Card::new($crate::card_color!($color), $crate::card_value!($value))
+    };
+}
+
 #[derive(Debug, Clone, Copy)]
 pub struct Card {
-    color: CardColor,
-    value: CardValue,
+    _color: CardColor,
+    _value: CardValue,
 }
 
 impl Card {
-    pub fn new(color: CardColor, value: CardValue) -> Self {
-        Self { color, value }
+    pub fn new(_color: CardColor, _value: CardValue) -> Self {
+        Self { _color, _value }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-use hanabi_tui::{
-    card::{Card, color::CardColor, value::CardValue},
-    card_color, card_value,
-    hand::Hand,
-};
+use hanabi_tui::{card, hand::Hand};
 
 fn main() {
-    let _card = Card::new(card_color!("blue"), card_value!(3));
+    let _card = card!("red", 2);
     let _hand = Hand::new([_card; 5]);
 
     println!("{:?}", _hand);
