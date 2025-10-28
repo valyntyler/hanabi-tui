0d7e9b6bc79d25378c503f7275ef9add2b71be03
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-10-29 00:01:37 +0100

    feat: card_value! macro

--- src/card/value/mod.rs
+++ src/card/value/mod.rs
@@ -1,3 +1,22 @@
+#[macro_export]
+macro_rules! card_value {
+    (1) => {
+        CardValue::_1
+    };
+    (2) => {
+        CardValue::_2
+    };
+    (3) => {
+        CardValue::_3
+    };
+    (4) => {
+        CardValue::_4
+    };
+    (5) => {
+        CardValue::_5
+    };
+}
+
 #[derive(Debug, Clone, Copy)]
 pub enum CardValue {
     _1,
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use hanabi_tui::{
     card::{Card, color::CardColor, value::CardValue},
-    card_color,
+    card_color, card_value,
     hand::Hand,
 };
 
 fn main() {
-    let _card = Card::new(card_color!("blue"), CardValue::_5);
+    let _card = Card::new(card_color!("blue"), card_value!(3));
     let _hand = Hand::new([_card; 5]);
 
     println!("{:?}", _hand);
