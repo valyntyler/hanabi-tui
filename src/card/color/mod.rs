b61315b4f525cc8d3777a39caf17f769f962751f
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-10-28 23:58:33 +0100

    feat: card_color! macro

--- src/card/color/mod.rs
+++ src/card/color/mod.rs
@@ -1,3 +1,22 @@
+#[macro_export]
+macro_rules! card_color {
+    ("blue") => {
+        CardColor::Blue
+    };
+    ("green") => {
+        CardColor::Green
+    };
+    ("red") => {
+        CardColor::Red
+    };
+    ("white") => {
+        CardColor::White
+    };
+    ("yellow") => {
+        CardColor::Yellow
+    };
+}
+
 #[derive(Debug, Clone, Copy)]
 pub enum CardColor {
     Blue,
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use hanabi_tui::{
     card::{Card, color::CardColor, value::CardValue},
+    card_color,
     hand::Hand,
 };
 
 fn main() {
-    let _card = Card::new(CardColor::Red, CardValue::_5);
+    let _card = Card::new(card_color!("blue"), CardValue::_5);
     let _hand = Hand::new([_card; 5]);
 
     println!("{:?}", _hand);
