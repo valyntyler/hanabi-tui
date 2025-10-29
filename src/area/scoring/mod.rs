ed44518003a8a9d94be6d8560bf227b40eb15e27
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-10-29 12:12:39 +0100

    feat: impl ScoringArea::default

--- src/area/scoring/mod.rs
+++ src/area/scoring/mod.rs
@@ -1,4 +1,16 @@
-use crate::pile::scoring::ScoringPile;
+use crate::{card::color::CardColor, pile::scoring::ScoringPile};
 
 #[allow(dead_code)]
 pub struct ScoringArea([ScoringPile; 5]);
+
+impl Default for ScoringArea {
+    fn default() -> Self {
+        Self([
+            ScoringPile::new(None, CardColor::Blue),
+            ScoringPile::new(None, CardColor::Green),
+            ScoringPile::new(None, CardColor::Red),
+            ScoringPile::new(None, CardColor::White),
+            ScoringPile::new(None, CardColor::Yellow),
+        ])
+    }
+}
