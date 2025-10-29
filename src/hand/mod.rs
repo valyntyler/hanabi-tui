c9aaedf5aaf95ce4226b338eed556ba76aaa621f
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-10-29 01:08:32 +0100

    refactor: hand size now variable

--- src/hand/mod.rs
+++ src/hand/mod.rs
@@ -2,10 +2,10 @@ use crate::card::Card;
 
 #[derive(Debug)]
 #[allow(dead_code)]
-pub struct Hand([Card; 5]);
+pub struct Hand(Vec<Card>);
 
 impl Hand {
-    pub fn new(cards: [Card; 5]) -> Self {
+    pub fn new(cards: Vec<Card>) -> Self {
         Hand(cards)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use hanabi_tui::{card, deck::Deck, hand::Hand};
 
 fn main() {
-    let _card = card!("red", 2);
-    let _deck = Deck::default();
-    let _hand = Hand::new([_card; 5]);
+    let mut _deck = Deck::default();
 
-    println!("{:#?}", _deck);
+    let _card = card!("red", 2);
+    let _hand = Hand::new(vec![
+        _deck.draw().unwrap(),
+        _deck.draw().unwrap(),
+        _deck.draw().unwrap(),
+        _deck.draw().unwrap(),
+        _deck.draw().unwrap(),
+    ]);
+
+    println!("{:#?}", _hand);
 }
